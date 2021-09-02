624439dd2614427fce3b9a5056963607be7da481
agouai <suoan@haier.com>
2021-09-02 16:58:53 +0800

    vue造轮子

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <FirstComponents/>
+</template>
+
+<script>
+import FirstComponents from './components/FirstComponents.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FirstComponents
+  }
+}
+</script>
